5af18e98ca591d0f2aa49a25638174e62461f563
sanja <sanja1mandic@gmail.com>
2022-12-08 12:19:27 +0100

    Launches endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const planetRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launches.router')
 const cors = require('cors');
 const morgan = require('morgan');
 
@@ -13,6 +14,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetRouter);
+app.use(launchesRouter);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
